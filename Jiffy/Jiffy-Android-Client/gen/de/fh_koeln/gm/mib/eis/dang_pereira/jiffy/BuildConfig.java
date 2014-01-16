449f8af62120b8e2590d8149cb58952846a824d0
Ngoc-Anh D. <dangngocanh24@yahoo.com>
2014-01-16 22:05:30 +0100

    deleted no2

--- Jiffy/Jiffy-Android-Client/gen/de/fh_koeln/gm/mib/eis/dang_pereira/jiffy/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package de.fh_koeln.gm.mib.eis.dang_pereira.jiffy;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
